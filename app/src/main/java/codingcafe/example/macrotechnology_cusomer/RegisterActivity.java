58dd219b427dcad6d5f0e1ac80f64012616b3ab4
RPIUdishan <it18082234@my.sliit.lk>
2019-08-06 00:05:43 +0530

    Customer GUI created

---
+++ app/src/main/java/codingcafe/example/macrotechnology_cusomer/RegisterActivity.java
@@ -0,0 +1,34 @@
+package codingcafe.example.macrotechnology_cusomer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class RegisterActivity extends AppCompatActivity {
+    private Button CreateAccountButton;
+    private EditText InputName,InputEmail,InputPassword;
+    private ProgressDialog loadingBar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        CreateAccountButton=(Button) findViewById(R.id.register_btn);
+        InputName=(EditText) findViewById(R.id.register_username_input);
+        InputPassword=(EditText) findViewById(R.id.register_password_input);
+        InputEmail=(EditText)findViewById(R.id.register_email_input);
+        loadingBar =new ProgressDialog(this );
+        CreateAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+    }
+}
